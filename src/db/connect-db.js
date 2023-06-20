4ca5290c32be4fc4efa52955f208b8f9f3c45889
Aditi571 <rawataditi571@gmail.com>
2023-06-20 15:39:29 +0530

    updated online db

--- src/db/connect-db.js
+++ src/db/connect-db.js
@@ -1,8 +1,10 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Studentdata",{
+mongoose.connect("mongodb+srv://b22ai004:JJVOraDr1EEyI5eT@cluster0.u15uqaf.mongodb.net/StudentData?retryWrites=true&w=majority",{
   useNewUrlParser:true,
+  
   useUnifiedTopology:true,
  
+  
 }).then(()=>{
   console.log("connection");
 }).catch((err)=>{
